0590fb086257c81634fb285682da8d6dafebecdf
Hachibkhan <hachib.khan19@gmail.com>
2019-05-04 20:30:50 +0600

    Triangle.py

---
+++ tri.py
@@ -0,0 +1,23 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.speed(2)
+
+turtle.color("green")
+
+turtle.forward(130)
+
+turtle.left(120)
+
+turtle.forward(130)
+
+turtle.left(120)
+
+turtle.forward(130)
+
+turtle.left(120)
+
+turtle.exitonclick()
+
+
